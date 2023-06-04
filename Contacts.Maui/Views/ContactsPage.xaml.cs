f84f0f870f86eda5c5b6040aa0efd17717acccac
Kyo <kyo2971@gmail.com>
2023-06-04 16:35:27 +0800

    [Finsh]

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -15,9 +15,11 @@ public partial class ContactsPage : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
+        SearchBar.Text = string.Empty;
+        LoadContacts();
+        //var contacts = new ObservableCollection<Contact>(ContactReposity.GetContacts());
+        //listContacts.ItemsSource = contacts;
 
-        var contacts = new ObservableCollection<Contact> (ContactReposity.GetContacts());
-        listContacts.ItemsSource = contacts;
     }
 
     private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -35,4 +37,30 @@ public partial class ContactsPage : ContentPage
         //DisplayAlert("test", "test", "ok");
         listContacts.SelectedItem = null;
     }
+
+    private void btnAdd_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+
+    private void Delete_Clicked(object sender, EventArgs e)
+    {
+        var menuItem=sender as MenuItem;
+        var contact= menuItem.CommandParameter as Contact;
+        ContactReposity.DeleteContact(contact.ContactId);
+        LoadContacts();
+    }
+
+    private void LoadContacts()
+    {
+        var contacts = new ObservableCollection<Contact>(ContactReposity.GetContacts());
+        listContacts.ItemsSource = contacts;
+    }
+
+    private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var contacts = new ObservableCollection<Contact>(ContactReposity.SearchContacts(((SearchBar)sender).Text));
+        listContacts.ItemsSource = contacts;
+    }
+
 }
